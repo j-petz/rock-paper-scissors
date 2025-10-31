a37bbabdbf714a70c214232a6db59c8b960b819c
jpetz <60827828+j-petz@users.noreply.github.com>
2025-10-31 13:53:15 +0100

    Add function for computerChoice
    Created a mathematical object to get a number between 1 and 99.
    Created a function for the computerChoice.
    If the number is smaller or equal 33 return 'rock'.
    If the number is smaller or equal 66 return 'paper'.
    In any other case return 'scissors'.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello Joseph!");
+// Find number between 1 and 99
+let rand = Math.floor(Math.random() * 99 + 1);
+
+console.log(rand);
+
+// Create function with the name 'computerChoice'
+function computerChoice() {
+    // If the number is 33 or less return the value 'Rock'
+    if (rand <= 33) {
+        return "rock";
+    } else if (rand <= 66) {
+        // If the number is 66 or less return the value 'Paper'
+        return "paper";
+    } else {
+        // In any other case return the value 'Scissors'
+        return "scissors";
+    }
+}
+
+console.log(computerChoice());
