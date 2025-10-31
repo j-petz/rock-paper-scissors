e5dab2213ab5107ac951b7ff319dae4bdf75aac5
jpetz <60827828+j-petz@users.noreply.github.com>
2025-10-31 14:30:32 +0100

    Add function to get the choice of the user
    Created function getUserChoice. It asks the user to enter Rock, Paper or Scissors in a prompt.
    Then it transforms the input value into lowercase (in case the user spelled it with an uppercase letter).
    It also checks if the user choice equals one of the needed terms and if not it logs a message to the console.

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let rand = Math.floor(Math.random() * 99 + 1);
 
 console.log(rand);
 
-// Create function with the name 'computerChoice'
-function computerChoice() {
+// Create function with the name 'getComputerChoice'
+function getComputerChoice() {
     // If the number is 33 or less return the value 'Rock'
     if (rand <= 33) {
         return "rock";
@@ -17,4 +17,29 @@ function computerChoice() {
     }
 }
 
-console.log(computerChoice());
+console.log(`Computer Choice: ${getComputerChoice()}`);
+
+// Create function for user choice
+function getUserChoice() {
+    // Ask user to enter Rock Paper or Scissors in a Prompt
+    let userInput = prompt(
+        "Choose Rock, Paper or Scissors",
+        "Which will you choose?"
+    ).toLowerCase();
+    // Transform the input to lowercase
+    console.log(`User Input: ${userInput}`);
+
+    // Check if the selection equals one of the terms
+    if (
+        !(
+            userInput === "rock" ||
+            userInput === "paper" ||
+            userInput === "scissors"
+        )
+    ) {
+        // If NOT log 'invalid input'
+        console.log("Invalid choice. Please select Rock, Paper or Scissors");
+    }
+}
+// Call function getUserChoice
+getUserChoice();
