4774cd4eb75e697c53ae8054e7035861dd12e0a8
jpetz <60827828+j-petz@users.noreply.github.com>
2025-12-18 15:39:56 +0100

    Add a restart button and some styling

--- script.js
+++ script.js
@@ -10,16 +10,18 @@ function getComputerChoice() {
     }
 }
 
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll(".wrapper button");
 buttons.forEach((button) => {
     button.addEventListener("click", playRound);
 });
 
 let userScore = 0;
 let computerScore = 0;
+let ties = 0;
+let rounds = 0;
 
-let result = document.querySelector("#result");
-result.setAttribute("style", "white-space: pre");
+let message = document.querySelector("#message");
+message.setAttribute("style", "white-space: pre");
 
 function playRound(e) {
     let userChoice = e.target.dataset.value;
@@ -28,32 +30,53 @@ function playRound(e) {
     let score = document.querySelector("#score");
     score.setAttribute("style", "white-space: pre");
 
-    result.textContent = `You chose: ${userChoice}. || The computer chose ${computerChoice}.`;
+    message.textContent = `You chose: ${userChoice}. || The computer chose ${computerChoice}.`;
 
     // Compare userChoice and computerChoice
     if (userChoice === "rock" && computerChoice === "scissors") {
-        result.textContent += `\r\nRock beats Scissors.\r\nYou won this round!`;
+        message.textContent += `\r\nRock beats Scissors.\r\nYou won this round!`;
         userScore++;
     } else if (userChoice === "paper" && computerChoice === "rock") {
-        result.textContent += `\r\nPaper beats Rock.\r\nYou won this round!`;
+        message.textContent += `\r\nPaper beats Rock.\r\nYou won this round!`;
         userScore++;
     } else if (userChoice === "scissors" && computerChoice === "paper") {
-        result.textContent += `\r\nScissors beats Paper.\r\nYou won this round!`;
+        message.textContent += `\r\nScissors beats Paper.\r\nYou won this round!`;
         userScore++;
     } else if (userChoice === computerChoice) {
-        result.textContent += `\r\nIt's a tie!`;
-    } else {
-        result.textContent += `\r\nYou lose this round!`;
+        message.textContent += `\r\nIt's a tie!`;
+        ties++;
+    } else if (computerChoice === "rock" && userChoice === "scissors") {
+        message.textContent += `\r\nRock beats Scissors.\r\nYou lose this round!`;
+        computerScore++;
+    } else if (computerChoice === "paper" && userChoice === "rock") {
+        message.textContent += `\r\nPaper beats Rock.\r\nYou lose this round!`;
+        computerScore++;
+    } else if (computerChoice === "scissors" && userChoice === "paper") {
+        message.textContent += `\r\nScissors beats Paper.\r\nYou lose this round!`;
         computerScore++;
     }
-    score.textContent = `User: ${userScore} || Computer: ${computerScore}`;
+    rounds++;
+    score.textContent = `User: ${userScore} || Computer: ${computerScore} || Ties: ${ties}`;
 
     // Declare the final winner
     if (userScore == 5) {
-        score.textContent += `\r\nYou won ${userScore} out of 5 rounds.\r\nYou are the winner of the match! 🥳`;
+        score.textContent += `\r\nYou won ${userScore} out of ${rounds} rounds.\r\nYou are the winner of the match! 🥳`;
         buttons.forEach((button) => (button.disabled = true));
     } else if (computerScore == 5) {
-        score.textContent += `\r\nThe computer won ${computerScore} out of 5 rounds.\r\nYou lose the match ☹️`;
+        score.textContent += `\r\nThe computer won ${computerScore} out of ${rounds} rounds.\r\nYou lose the match ☹️`;
         buttons.forEach((button) => (button.disabled = true));
     }
+}
+
+// TODO: add reset button
+let btnRestart = document.querySelector("#restart");
+btnRestart.addEventListener("click", restartGame);
+
+function restartGame() {
+    buttons.forEach((button) => (button.disabled = false));
+    userScore = 0;
+    computerScore = 0;
+    ties = 0;
+    message.textContent = "";
+    score.textContent = "";
 }
